db7b9998c7f7ec7592f22943337455059a7e8ee6
adolforrt <adolforrt@gmail.com>
2015-09-08 13:58:51 -0300

    ex 3

---
+++ if and switch/exercise 3.js
@@ -0,0 +1,29 @@
+var a, b ,c;
+a = parseInt(prompt("Enter an integer number: "),10);
+b = parseInt(prompt("Enter an integer number: "),10);
+c = parseInt(prompt("Enter an integer number: "),10);
+
+/*
+a = prompt("Enter an integer number: ");
+b = prompt("Enter an integer number: ");
+c = prompt("Enter an integer number: ");
+*/
+
+//assuming all values are different
+
+console.log("a: "+a +" b: "+b +" c: "+c);
+if(a > b && a > c){
+	alert(a+" is the biggest one");
+}
+else{
+	if( b > c ){
+		console.log(b);
+		console.log(b>a);
+		console.log(b>c);
+		alert(b+ " is the biggest one");
+		
+	}
+	else{
+			alert(c+ " is the biggest one");
+		}
+}
