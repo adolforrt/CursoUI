98bfdb33bdde80d2315c28807a11f5bf3526e700
adolforrt <adolforrt@gmail.com>
2015-09-14 21:27:30 -0300

    ex 7 strings

---
+++ Strings/exercise 7.js
@@ -0,0 +1,16 @@
+var address = new String();
+address = prompt("Enter a URL: ");
+
+
+function jsonify(address){
+	var protocol = address.slice(0, address.indexOf(":"));
+	address = address.slice(address.indexOf(":")+3);
+	var server = address.slice(0,address.indexOf("/"));
+	var source = server.slice(server.indexOf("/"));
+	var jaddress = 
+					var text = '{ "employees" : [' +
+'{ "firstName":"John" , "lastName":"Doe" },' +
+'{ "firstName":"Anna" , "lastName":"Smith" },' +
+'{ "firstName":"Peter" , "lastName":"Jones" } ]}'; 
+				
+}
