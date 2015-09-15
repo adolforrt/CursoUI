2cd88a9b0872de9e001337f39bd22782c2db35bf
adolforrt <adolforrt@gmail.com>
2015-09-15 10:41:09 -0300

    finally working

--- Strings/exercise 7.js
+++ Strings/exercise 7.js
@@ -1,16 +1,12 @@
 var address = new String();
-address = prompt("Enter a URL: ");
-
+address = prompt("Enter a URL (i.e: https://www.google.com/index.aspx): ");
+var jAdress = jsonify(address);
 
 function jsonify(address){
 	var protocol = address.slice(0, address.indexOf(":"));
 	address = address.slice(address.indexOf(":")+3);
 	var server = address.slice(0,address.indexOf("/"));
-	var source = server.slice(server.indexOf("/"));
-	var jaddress = 
-					var text = '{ "employees" : [' +
-'{ "firstName":"John" , "lastName":"Doe" },' +
-'{ "firstName":"Anna" , "lastName":"Smith" },' +
-'{ "firstName":"Peter" , "lastName":"Jones" } ]}'; 
-				
+	var resource = address.slice(address.indexOf("/"));
+	
+	return JSON.parse('{ "protocol":"'+protocol+'","server":"'+server+'","resource":"'+resource+'" }');
 }
