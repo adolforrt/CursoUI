cb993b45b9552cedfa127b76a426a173450c5899
adolforrt <adolforrt@gmail.com>
2015-09-18 19:32:20 -0300

    carousel working. Footer OK. pills under construction.

--- Comics Project/js/index.js
+++ Comics Project/js/index.js
@@ -4,37 +4,46 @@ function loadUser(){
 	var username = jsonObj.justSigned.username;
 	document.getElementById("user").innerHTML = "Hello "+ '<a  href="myAccount.html">'+username+'</a>';
 	markup();
+	pills();
 }
 
 	
-	$('#myTabs a').click(function (e) {
-	e.preventDefault()
-	$(this).tab('show')
+function pills(){
+$(".nav.nav-pills.nav-stacked li").on("click",function(){
+  $(".nav.nav-pills li").removeClass("active");
+  $(this).addClass("active");
+});
+	
+}
+
+$('#myTabs a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
 })
 
+
+
+
+
 function markup(){
-	console.log("MARKUP");
-	//retrieve
+	
+	//retrieve data
 	var jsonString = localStorage.getItem("list"); 
 	var jsonObj = JSON.parse(jsonString);
-	console.log(jsonString);
-	console.log(jsonObj);
-	console.log(jsonObj.comics.length);
 	
-	
+	//generate markup	
 	for(i=0; i<jsonObj.comics.length; i++){
 		var comic = '<div class="comic"><img src="'
 					+jsonObj.comics[i].pic+'" alt="" ><h3>'
 					+jsonObj.comics[i].name+'</h3><div>Available: '
 					+jsonObj.comics[i].avail+'</div><div>Genre: '
 					+jsonObj.comics[i].genre+'</div><div>Pages: '
-					+jsonObj.comics[i].pages+'</div><div>Format:'
+					+jsonObj.comics[i].pages+'</div><div>Format: '
 					+jsonObj.comics[i].format+'</div><div>Rent now!</div></div>';	
-		console.log(comic);
+		//inject within DOM
 		document.getElementById("comicsContainer").innerHTML += comic;
 		if(document.getElementById("comicsContainer") == null){
 			console.log("couldnt find container");
 		}
-		
 	}
 }	
