c801d8c001bdab60b71aa800496aaeb39696e28b
adolforrt <adolforrt@gmail.com>
2015-09-19 12:52:01 -0300

    Register script changes

--- Comics Project/js/registration.js
+++ Comics Project/js/registration.js
@@ -1,24 +1,32 @@
 function register(form) {
-				
-//FIRST TIME
-if (!localStorage.getItem("comics")) {
-	alert("its the first time");
-	localStorage.setItem("comics", firstTime(form));
-	
-}
-	//STORE				
-	if (typeof(Storage) !== "undefined") {
-			localStorage.setItem("comics", store(form));
-	} else {
-			document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+
+if(form.pass.value != ""){
+	if(form.pass.value != form.pass2.value){
+		alert("Passwords are not the same");
 	}
-	location = "login.html";
+}	
+else{
+	//FIRST TIME
+	if (!localStorage.getItem("comics")) {
+		alert("its the first time");
+		localStorage.setItem("comics", firstTime(form));
+		
+	}else{
+		//STORE				
+		if (typeof(Storage) !== "undefined") {
+				localStorage.setItem("comics", store(form));
+		} else {
+				document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+		}
+		
+	}
+		location = "login.html";
+	}						
 }
 
 
 
 function firstTime(form){
-		
 	var jTemp = JSON.parse('{ 	"justSigned": { "username": "'+form.id.value+'" , "password": "'+form.pass.value+'","type":"user"}, "users": [{"username":"'+form.id.value+'","password":"'+form.pass.value+'","type":"user"}]}');
 	var jsonString = JSON.stringify(jTemp);
 	console.log("users length: "+jTemp.users.length +"  elements: ");
@@ -54,14 +62,16 @@ function store(form){
 }
 
 
+	
+
 
 /*
 	just for simplicity, avoiding the use of a web server
-	it'd better to load a JSON
+	it'd better to load a JSON ;)
 */
 function comics(){
 	var list = '{	"comics": [{"pic":"img/spiderman_thumb.jpg", "name" :"Spiderman", "genre":"SuperHeroe", "format":"Paper", "pages":"128", "avail":"50"},{"pic":"img/venom_thumb.jpg", "name" :"Venom", "genre":"SuperHeroe", "format":"Ebook", "pages":"160", "avail":"80"},{"pic":"img/storm_thumb.jpg", "name" :"Storm", "genre":"SuperHeroe", "format":"Paper", "pages":"210", "avail":"11"},{"pic":"img/wonderwoman_thumb.jpg", "name" :"Wonder Woman", "genre":"SuperHeroe", "format":"Paper", "pages":"115", "avail":"8"},{"pic":"img/superman_thumb.jpg", "name" :"Superman", "genre":"SuperHeroe", "format":"Ebook", "pages":"300", "avail":"15"},{"pic":"img/wolverine_thumb.jpg", "name" :"Wolverine", "genre":"SuperHeroe", "format":"Paper", "pages":"190", "avail":"14"},{"pic":"img/hulk_thumb.jpg", "name" :"The incredible Hulk", "genre":"SuperHeroe", "format":"Ebook", "pages":"220", "avail":"80"},{"pic":"img/blackwidow_thumb.jpg", "name" :"Black Widow", "genre":"SuperHeroe", "format":"Ebook", "pages":"200", "avail":"80"},{"pic":"img/mystique_thumb.jpg", "name" :"Mystique", "genre":"SuperHeroe", "format":"Ebook", "pages":"160", "avail":"80"}]}';
-	//store
+	
 	//STORE				
 	if (typeof(Storage) !== "undefined") {
 			localStorage.setItem("list", list);
@@ -86,4 +96,3 @@ function comics(){
 	
 }
 	
-// hay un error, se estan guardando dos veces los usuarios al momento de la registracion
